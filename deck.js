24a000d01355f283cdc80dc9a6764b20c1ad2268
ChargeAhead <iregazzoli@fi.uba.ar>
2022-12-19 18:45:06 -0300

    Add function to create decks

--- deck.js
+++ deck.js
@@ -1,27 +1,56 @@
 export default class Deck {
-  constructor(cards) {
-    this.cards = cards;
+  constructor(cardsFeature, quantities) {
+    this.cards = addCards(cardsFeature, quantities); //cards and quantities are an array of objects
   }
 }
 
+function addCards(cardsFeature, quantities) {
+  //maybe it should be a method?
+  const cards = [];
+  for (const cardSpecs of quantities) {
+    for (const cardInfo of cardsFeature) {
+      if (cardInfo.id === cardSpecs.id) {
+        for (let i = 0; i < cardSpecs.copies; i++) {
+          cards.push(createCard(cardInfo));
+        }
+      }
+    }
+  }
+  return cards;
+}
+
+function createCard(cardFeatures) {
+  let cardType = cardFeatures.type;
+  let cardName = cardFeatures.name;
+  let cardCost = cardFeatures.cost;
+  let cardColor = cardFeatures.color;
+
+  if (cardType === "mineral") {
+    let cardAtk = cardFeatures.atk;
+    let cardDef = cardFeatures.def;
+    return new Creature(cardType, cardName, cardCost, cardColor, cardAtk, cardDef);
+  } else if (cardType === "gem") return new Mana(cardType, cardName, cardCost, cardColor);
+}
+
 class Card {
-  constructor(type, name, cost) {
+  constructor(type, name, cost, color) {
     this.type = type;
     this.name = name;
     this.cost = cost;
+    this.color = color;
   }
 }
 
 class Creature extends Card {
-  constructor(type, name, cost, atk, def) {
-    super(type, name, cost);
+  constructor(type, name, cost, color, atk, def) {
+    super(type, name, cost, color);
     this.atk = atk;
     this.def = def;
   }
 }
 
 class Mana extends Card {
-  constructor(type, name, cost) {
-    super(type, name, cost);
+  constructor(type, name, cost, color) {
+    super(type, name, cost, color);
   }
 }
--- script.js
+++ script.js
@@ -1,4 +1,37 @@
-import Deck from "deck.js";
+import Deck from "./deck.js";
 
-const deck = new Deck();
+let cards = [
+  {
+    id: 1,
+    type: "mineral",
+    name: "Phosphophyllite",
+    cost: 1,
+    color: "green",
+    def: 3,
+    atk: 1,
+  },
+
+  {
+    id: 2,
+    type: "gem",
+    name: "Esmeral",
+    cost: 0,
+    color: "green",
+  },
+];
+
+let playerDeck = [
+  {
+    id: 1,
+    copies: 3,
+  },
+
+  {
+    id: 2,
+    copies: 2,
+  },
+];
+// console.log(cards);
+// console.log(playerDeck);
+const deck = new Deck(cards, playerDeck);
 console.log(deck.cards);
